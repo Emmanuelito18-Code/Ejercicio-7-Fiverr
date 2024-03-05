32894d5b1824003d804f6cb66667ad544a88637f
beto200224 <pepebetolop@gmail.com>
2024-03-04 23:40:32 -0600

    commit_Inicial

--- src/edificio/Edificio.java
+++ src/edificio/Edificio.java
@@ -8,27 +8,27 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Calendar;
 public class Edificio {
-  static Calendar Fecha = Calendar.getInstance();
-  static Calendar Hora = Calendar.getInstance();
+  static Calendar Fecha = Calendar.getInstance();//funcion para ingresar fecha actual
+  static Calendar Hora = Calendar.getInstance();// funcion para agregar hora actual
   static Scanner scanner =new Scanner(System.in);
     
   public static void main (String[] args ){
     int opcion = 0;
-    do{    
+    do{    //menu principal
     System.out.println("Bienvenido a la resepcion del edificio ");
     System.out.println("que desea hacer");
-    System.out.println("1- registrara entrada           2- marcar salida          3-salir del progrma");
+    System.out.println("1- registrar entrada           2- marcar salida          3-salir del progrma");
     System.out.println("digite el numero de la operacion");
-    opcion=scanner.nextInt();
+    opcion=scanner.nextInt();//ingreso de funcion a realizar
         switch(opcion){
             case 1:
-                registro();
+                registro();//funcion para ingresar datos del visitante
                 break;
             case 2:
-             Eliminar();  
+             Eliminar();  //funcion para eliminar visita
              break;
                 
-            default:
+            default://salida del programa
                 
         }
     }while(opcion!=3);
@@ -36,28 +36,33 @@ public class Edificio {
       static void registro() {
      Scanner leer = new Scanner(System.in);
      System.out.print("Ingrese un DNI:  ");
-     int DNI = leer.nextInt();
+     int DNI = leer.nextInt(); //introduccion de DNI del visitante
      ArrayList listado = new ArrayList();
-     listado.add(1010);
+     String nombre;//variable donde se almacenan los nombres del visitante
+     Scanner teclado = new Scanner(System.in);
+     System.out.print("Ingrese su nombre completo:  ");
+      nombre = teclado.nextLine();//metodo para leer la variable
+      System.out.println("Nombre del visitante " + nombre);
+     listado.add(1010);//DNI'S ya registrados 
      listado.add(2020);
-     int min, hora, dia, mes, anno;
-     String FechaActual, HoraActual;
-     hora = Hora.get(Calendar.HOUR_OF_DAY);
-     min = Hora.get(Calendar.MINUTE);
-     dia = Fecha.get(Calendar.DATE);
-     mes = Fecha.get(Calendar.MONTH);
-     anno = Fecha.get(Calendar.YEAR);
-     HoraActual = hora + ":" + min;
-     FechaActual = dia + "/" + (mes+1) + "/" + anno;
+     int min, hora, dia, mes, anno;//variantes de fecha y hora local
+     String FechaActual, HoraActual;//cadena de hora y fecha
+     hora = Hora.get(Calendar.HOUR_OF_DAY);//llamado de hora del sistema
+     min = Hora.get(Calendar.MINUTE);//llamado de minutos transcuridos
+     dia = Fecha.get(Calendar.DATE);//llamado del dia actual
+     mes = Fecha.get(Calendar.MONTH);//llamado del mes actual
+     anno = Fecha.get(Calendar.YEAR);//llamado del año actual 
+     HoraActual = hora + ":" + min;//union de la hora completa
+     FechaActual = dia + "/" + (mes+1) + "/" + anno;//union de la fecha completa
         System.out.println("La fecha del dia es: "+ FechaActual +" Hora del registro: "+ HoraActual);
         
      if(listado.contains(DNI)){
-        System.out.println("E DNI ya esta en el listado");
+        System.out.println("E DNI ya esta en el listado");//DNI registrado
      }else{
            listado.add(DNI);
-           System.out.println("El DNI fue guardado");
+           System.out.println("El DNI fue guardado");//DNI dado de alta
      }
-     System.out.println("lista; "+listado);
+     System.out.println("lista; "+listado);//listado de visitantes actuales 
     }
       static void Eliminar() {
      
